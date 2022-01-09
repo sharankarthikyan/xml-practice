f97f9de9302dfc6fff73b0a7aa5220ad563d4318
sharan karthikeyan <karthikeyansharan00@gmail.com>
2022-01-09 18:48:21 +0530

    sending xml file as response in XMLHttpRequest's response

--- public/script.js
+++ public/script.js
@@ -1,10 +1,26 @@
+function myFunction(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var table = '<tr><th>Director</th><th>Movie</th></tr>';
+  var x = xmlDoc.getElementsByTagName('movie');
+  for (i = 0; i < x.length; i++) {
+    table +=
+      '<tr><td>' +
+      x[i].getElementsByTagName('director')[0].childNodes[0].nodeValue +
+      '</td><td>' +
+      x[i].getElementsByTagName('movieName')[0].childNodes[0].nodeValue +
+      '</td></tr>';
+  }
+  document.getElementById('demo').innerHTML = table;
+}
+
 function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById('demo').innerHTML = this.responseText;
+      myFunction(this);
     }
   };
-  xhttp.open('GET', 'ajax_info.txt', true);
+  xhttp.open('GET', 'movies.xml', true);
   xhttp.send();
 }
